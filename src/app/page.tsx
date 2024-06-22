01b23b2fd4de2ea7b8c9653ad714558f1c981481
Jaesung Nam <overpowerman@gmail.com>
2024-06-22 15:58:35 +0900

    qweqw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,37 @@
 export default function Home() {
   return (
+    <div>
     <div style={{fontFamily: 'My Font', fontWeight: 500}}>
-      aaaa
+      qqwwefew
+    </div>
+      <div>
+        <div>
+          <button onClick={() => {
+            fetch('https://target.overpowerman.click/api/user', {method: 'GET'})
+          }}>get
+          </button>
+          <button onClick={() => {
+            fetch('https://target.overpowerman.click/api/user', {
+              method: 'POST', headers: {
+                "Content-Type": "application/json",
+              }, body: JSON.stringify({user: 1})
+            })
+          }}>post
+          </button>
+          <button onClick={() => {
+            fetch('https://target.overpowerman.click/api/user', {
+              method: 'PUT', headers: {
+                "Content-Type": "application/json",
+              }, body: JSON.stringify({user: 1})
+            })
+          }}>put
+          </button>
+          <button onClick={() => {
+            fetch('https://target.overpowerman.click/api/user', {method: 'DELETE'})
+          }}>get
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
